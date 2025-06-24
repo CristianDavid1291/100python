a00b10ce31b4106901d1adf701aeed5a4f97f630
Cristian <cristiandavid1291@gmail.com>
2025-06-24 11:07:03 -0500

    day 1

---
+++ day1_variables/day1Python.py
@@ -0,0 +1,2 @@
+print("Welcome to the Band Name Generator!")
+print("The name of your band is..." + " " + input("What is the name of the city you grew up in?") + " " + input("What is the name of your pet?"))
